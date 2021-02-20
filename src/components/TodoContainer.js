5c390571cb8ea0453da4a97f8830af0bc4cadd36
sandron <xoxol4uk22@gmail.com>
2021-02-20 10:45:46 +0200

    Changed order ot tasks and added keydown event (Enter)

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,88 +1,95 @@
-import React, { useState } from 'react'
-import FilterSearch from './FilterSearch';
-import TodoForm from './TodoForm';
-import TodoList from './TodoList';
+import React, { useState } from "react";
+import FilterSearch from "./FilterSearch";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 const TodoContainer = () => {
-    const [todos, setTodos] = useState([]);
-    const [filterValue, setfilterValue] = useState('');
-    const [searchValue, setSearchValue] = useState('');
-    const [sorthValue, seteSortValue] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [filterValue, setfilterValue] = useState("");
+  const [searchValue, setSearchValue] = useState("");
+  const [sorthValue, seteSortValue] = useState("");
 
-    const addTodo = (todoValue, priorityValue) => {
-        if (todoValue !== '') {
-            let newTodo = {
-                text: todoValue,
-                id: Math.floor(Math.random() * 10000),
-                priority: +priorityValue,
-                completed: false
-            }
-            setTodos([...todos, newTodo])
-        }
+  const addTodo = (todoValue, priorityValue) => {
+    if (todoValue !== "") {
+      let newTodo = {
+        text: todoValue,
+        id: Math.floor(Math.random() * 10000),
+        priority: +priorityValue,
+        completed: false,
+      };
+      setTodos([newTodo, ...todos]);
     }
+  };
 
-    const sortTodos = (todos) => {
-        let todosSorted = [...todos]
-        if (sorthValue === 'ascending') {
-            todosSorted.sort((a, b) => (a.priority > b.priority) ? 1 : (b.priority > a.priority) ? -1 : 0)
-            return todosSorted
-        }
-        if (sorthValue === 'descending') {
-            todosSorted.sort((a, b) => (a.priority < b.priority) ? 1 : (b.priority < a.priority) ? -1 : 0)
-            return todosSorted
-        }
-        return todos
+  const sortTodos = (todos) => {
+    let todosSorted = [...todos];
+    if (sorthValue === "ascending") {
+      todosSorted.sort((a, b) =>
+        a.priority > b.priority ? 1 : b.priority > a.priority ? -1 : 0
+      );
+      return todosSorted;
     }
-
-    const filterSearchTodos = (todos) => {
-        console.log()
-        let todosFiltered = [...todos]
-        if (filterValue || searchValue) {
-            if (searchValue) {
-                todosFiltered = todos.filter((item) => item.text.includes(searchValue))
-            }
-            if (filterValue) {
-                todosFiltered = todosFiltered.filter((item) => +item.completed === +filterValue)
-            }
-            return todosFiltered
-        }
-        return todos
+    if (sorthValue === "descending") {
+      todosSorted.sort((a, b) =>
+        a.priority < b.priority ? 1 : b.priority < a.priority ? -1 : 0
+      );
+      return todosSorted;
     }
+    return todos;
+  };
 
-    const removeTodo = (id) => {
-        const newTodoList = todos.filter((item) => item.id !== id);
-        setTodos(newTodoList);
-    };
-
-    const statusChangeTodo = (id) => {
-        let newTodoList = []
-        todos.forEach((item, index) => {
-            newTodoList[index] = todos[index]
-            if (item.id === +id) {
-                newTodoList[index].completed = !todos[index].completed
-            }
-        })
-        setTodos(newTodoList)
-        console.log(newTodoList)
+  const filterSearchTodos = (todos) => {
+    console.log();
+    let todosFiltered = [...todos];
+    if (filterValue || searchValue) {
+      if (searchValue) {
+        todosFiltered = todos.filter((item) => item.text.includes(searchValue));
+      }
+      if (filterValue) {
+        todosFiltered = todosFiltered.filter(
+          (item) => +item.completed === +filterValue
+        );
+      }
+      return todosFiltered;
     }
+    return todos;
+  };
 
-    return (
-        <>
-            <TodoForm addTodo={addTodo} />
-            <FilterSearch
-                filterValue={filterValue}
-                searchValue={searchValue}
-                setfilterValue={setfilterValue}
-                setSearchValue={setSearchValue}
-                sorthValue={sorthValue}
-                seteSortValue={seteSortValue}
-            />
-            <TodoList
-                todos={ sortTodos(filterSearchTodos(todos))}
-                removeTodo={removeTodo}
-                statusChangeTodo={statusChangeTodo} />
-        </>
-    )
-}
+  const removeTodo = (id) => {
+    const newTodoList = todos.filter((item) => item.id !== id);
+    setTodos(newTodoList);
+  };
 
-export default TodoContainer
+  const statusChangeTodo = (id) => {
+    let newTodoList = [];
+    todos.forEach((item, index) => {
+      newTodoList[index] = todos[index];
+      if (item.id === +id) {
+        newTodoList[index].completed = !todos[index].completed;
+      }
+    });
+    setTodos(newTodoList);
+    console.log(newTodoList);
+  };
+
+  return (
+    <>
+      <TodoForm addTodo={addTodo} />
+      <FilterSearch
+        filterValue={filterValue}
+        searchValue={searchValue}
+        setfilterValue={setfilterValue}
+        setSearchValue={setSearchValue}
+        sorthValue={sorthValue}
+        seteSortValue={seteSortValue}
+      />
+      <TodoList
+        todos={sortTodos(filterSearchTodos(todos))}
+        removeTodo={removeTodo}
+        statusChangeTodo={statusChangeTodo}
+      />
+    </>
+  );
+};
+
+export default TodoContainer;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,48 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const TodoForm = ({ addTodo }) => {
+  const [todoValue, setTodoValue] = useState("");
+  const [priorityValue, setPriorityValue] = useState(1);
 
-
-    const [todoValue, setTodoValue] = useState('');
-    const [priorityValue, setPriorityValue] = useState(1);
-
-    const handleChange = (e) => {
-        if(e.target.name === 'textTodoInput') {
-            setTodoValue(e.target.value)
-        }
-        if(e.target.name === 'textPriorityInput') {
-            setPriorityValue(e.target.value)
-        }
+  const handleChange = (e) => {
+    if (e.target.name === "textTodoInput") {
+      setTodoValue(e.target.value);
     }
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        addTodo(todoValue, priorityValue)
-        setTodoValue('')
+    if (e.target.name === "textPriorityInput") {
+      setPriorityValue(e.target.value);
     }
+  };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addTodo(todoValue, priorityValue);
+    setTodoValue("");
+  };
 
-    return (
-        <div className='todoForm' >
-            <input
-                type='text'
-                name='textTodoInput'
-                placeholder='enter task'
-                onChange={handleChange}
-                value={todoValue} />
-            <button className='todoButton' type='submit' onClick={onSubmit}>add</button>
-            <input
-                type='range'
-                name='textPriorityInput'
-                min='1'
-                max='10'
-                step='1'
-                onChange={handleChange}
-                value={priorityValue} />
-                <div className='priorityValue'>Priority: {priorityValue}</div>
-        </div>
-    )
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onSubmit(e);
+    }
+  };
 
-}
+  return (
+    <div className="todoForm">
+      <input
+        type="text"
+        name="textTodoInput"
+        placeholder="enter task"
+        onChange={handleChange}
+        value={todoValue}
+        onKeyDown={handleKeyDown}
+      />
+      <button className="todoButton" type="submit" onClick={onSubmit}>
+        add
+      </button>
+      <input
+        type="range"
+        name="textPriorityInput"
+        min="1"
+        max="10"
+        step="1"
+        onChange={handleChange}
+        value={priorityValue}
+      />
+      <div className="priorityValue">Priority: {priorityValue}</div>
+    </div>
+  );
+};
 
-export default TodoForm
+export default TodoForm;
