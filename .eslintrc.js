4deb8b6a621352db5bdfeae574308629ea9bbe70
sandron <xoxol4uk22@gmail.com>
2021-02-22 22:25:43 +0200

    fixed problem with compatibility eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-/* module.exports = {
+module.exports = {
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   env: {
     es6: true,
@@ -8,6 +8,3 @@
     ecmaVersion: 8,
   },
 };
- */
-
-//comment because get error "ailed to load plugin 'prettier' declared in '.eslintrc.js': Cannot find module 'eslint-plugin-prettier'"
